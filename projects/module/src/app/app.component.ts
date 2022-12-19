a4792eaca8a0844c47e0c52cf7943e08a8e8e704
Jakub Kovář <6195502+minop@users.noreply.github.com>
2022-12-19 16:23:07 +0100

    model & spec upload

--- projects/module/src/app/app.component.ts
+++ projects/module/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {DropFile, FD_PETRI_NET, PetriNetParserService} from 'ilpn-components';
+import {DropFile, FD_PETRI_NET, PetriNet, PetriNetParserService} from 'ilpn-components';
 
 @Component({
     selector: 'app-root',
@@ -9,14 +9,29 @@ import {DropFile, FD_PETRI_NET, PetriNetParserService} from 'ilpn-components';
 export class AppComponent {
 
     public readonly FD_PN = FD_PETRI_NET;
+
+    public model: PetriNet | undefined;
+    public specs: Array<PetriNet> = [];
+
     constructor(private _parser: PetriNetParserService) {
     }
 
     modelUpload(files: Array<DropFile>) {
-        console.log(files);
+        this.model = this._parser.parse(files[0].content);
+        console.debug('model', this.model);
+        this.computeTokenTrails();
     }
 
     specUpload(files: Array<DropFile>) {
-        console.log(files);
+        this.specs = files.map(f => this._parser.parse(f.content)).filter(v => !!v) as Array<PetriNet>;
+        console.debug('specs', this.specs);
+        this.computeTokenTrails();
+    }
+
+    private computeTokenTrails() {
+        if (this.model === undefined || this.specs === undefined) {
+            return;
+        }
+
     }
 }
