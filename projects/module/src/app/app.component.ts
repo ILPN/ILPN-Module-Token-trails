965e31381eb16ecd0942a14248e667527355efb1
Jakub Kovář <6195502+minop@users.noreply.github.com>
2022-12-19 15:17:42 +0100

    module skeleton

--- projects/module/src/app/app.component.ts
+++ projects/module/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {FD_PETRI_NET, PetriNetParserService} from 'ilpn-components';
 
 @Component({
     selector: 'app-root',
@@ -6,4 +7,8 @@ import {Component} from '@angular/core';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+    public readonly FD_PN = FD_PETRI_NET;
+    constructor(private _parser: PetriNetParserService) {
+    }
 }
