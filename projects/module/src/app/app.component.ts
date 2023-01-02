97e7a85151c2d769c8311e805f9552bc5c33783c
Jakub Kovář <6195502+minop@users.noreply.github.com>
2023-01-02 13:45:56 +0100

    token trail validation skeleton

--- projects/module/src/app/app.component.ts
+++ projects/module/src/app/app.component.ts
@@ -1,6 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
 import {DropFile, FD_PETRI_NET, PetriNet, PetriNetParserService} from 'ilpn-components';
-import {BehaviorSubject, filter, map, Observable} from 'rxjs';
+import {BehaviorSubject, combineLatest, filter, map, Observable, Subscription} from 'rxjs';
 
 
 @Component({
@@ -8,7 +8,7 @@ import {BehaviorSubject, filter, map, Observable} from 'rxjs';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
 
     public readonly FD_PN = FD_PETRI_NET;
 
@@ -18,29 +18,36 @@ export class AppComponent {
     public modelNet$: Observable<PetriNet>;
     public specFirstNet$: Observable<PetriNet>;
 
+    private _latest$: Subscription;
+
     constructor(private _parser: PetriNetParserService) {
         this.model$ = new BehaviorSubject<PetriNet | undefined>(undefined);
         this.specs$ = new BehaviorSubject<Array<PetriNet>>([]);
         this.modelNet$ = this.model$.pipe(filter(v => v !== undefined)) as Observable<PetriNet>;
         this.specFirstNet$ = this.specs$.pipe(filter(v => v.length > 0), map(v => v[0]));
+        this._latest$ = combineLatest([this.model$, this.specs$]).subscribe( ([model, specs]) => {
+            if (model !== undefined && specs.length > 0) {
+                this.computeTokenTrails(model, specs);
+            }
+        });
+    }
+
+    ngOnDestroy(): void {
+        this._latest$.unsubscribe();
     }
 
     modelUpload(files: Array<DropFile>) {
         this.model$.next(this._parser.parse(files[0].content));
         console.debug('model', this.model$.value);
-        this.computeTokenTrails();
     }
 
     specUpload(files: Array<DropFile>) {
         this.specs$.next(files.map(f => this._parser.parse(f.content)).filter(v => !!v) as Array<PetriNet>);
         console.debug('specs', this.specs$.value);
-        this.computeTokenTrails();
     }
 
-    private computeTokenTrails() {
-        if (this.model$ === undefined || this.specs$ === undefined) {
-            return;
-        }
+    private computeTokenTrails(model: PetriNet, specs: Array<PetriNet>) {
+
 
     }
 }
